afec071f16c795077cf7f3d88fe6fc514eb63871
박기찬 <rl0425@naver.com>
2023-03-27 16:58:11 +0900

    Favorite 및 내가 쓴 글 페이지 무한 스크롤 적용

--- src/component/My/modal/FavoriteNote.js
+++ src/component/My/modal/FavoriteNote.js
@@ -7,19 +7,21 @@ import {v4 as uuidv4} from "uuid";
 import useHttp from "../../../hooks/use-http";
 import {modalActions} from "../../../store/modal-slice";
 
-// 무한스크롤
 import InfiniteScroll from "react-infinite-scroll-component";
-
 
 function FavoriteNote(){
     const [open, setOpen] = useState(false)
-    const [data, setData] = useState("")
+    const [data, setData] = useState([])
     const dispatch = useDispatch()
 
     // 무한 스크롤
     const [pageEnd, setPageEnd] = useState(true)
+    const [pageNum, setPageNum] = useState(0)
 
     const { isLoading, error, sendRequest: fetchTask } = useHttp();
+
+    // 모든 데이터의 로드 확인
+    const [dataLoaded, setDataLoaded] = useState(false);
 
     useEffect(()=>{
         setTimeout(()=> {
@@ -39,9 +41,9 @@ function FavoriteNote(){
     }
 
     const handleGetData = () => {
-        fetchTask({url: `http://explorer-cat-api.p-e.kr:8080/api/v1/post/bookmark/my`}, (taskObj) => {
-            console.log("taskObj = ", taskObj)
+        fetchTask({url: `http://explorer-cat-api.p-e.kr:8080/api/v1/post/bookmark/my?paging_num=${pageNum}&paging_count=20&sortType=desc`}, (taskObj) => {
             setData(taskObj)
+            setDataLoaded(true)
         })
     }
 
@@ -60,47 +62,86 @@ function FavoriteNote(){
         })
     }
 
-    const handleMoreData = (ele) => {
+    const handleMoreData = () => {
+        fetchTask({url: `http://explorer-cat-api.p-e.kr:8080/api/v1/post/bookmark/my?paging_num=${pageNum+1}&paging_count=20&sortType=desc`}, (taskObj) => {
+            if(taskObj.length > 0) {
+                const temp = [...data, ...taskObj]
+                setData(temp)
+            }
+            else{
+                setPageEnd(false)
+            }
+        })
 
+        setPageNum((prev) => prev+1)
     }
 
-    return (
-        data ?
-        <div className={open ? classes.box : classes.unBox}>
-            <div className={classes.head}>
-                <img onClick={handlePrevBtn} src={"/images/icons/prevBtn.png"}/>
-                <span>즐겨찾기한 노트 </span>
-            </div>
-            <div className={classes.body}>
-                <InfiniteScroll
-                    dataLength={data.length}
-                    next={() => handleMoreData(data)}
-                    hasMore={pageEnd}
-                    style={{ overflow: "scroll", height: "100%", display:"flex", flexDirection:"column", gap:"16px"}}
-                    loader={<h4>Loading...</h4>}
-                    height={"0"}
-                >
-                    {data.map((ele) => {
-                        return (
-                            <div onClick={() => handlePostDetail(ele)} key={uuidv4()} className={classes.content}>
 
-                                <div className={classes.contentBody}>
-                                    <div className={classes.contentTitle}>
-                                        <div className={classes.contentTitleSpan}><span>Q. {ele.title}</span></div>
-                                        <img onClick={(e) => handleIsFavorite(e,ele)} src={"/images/mypage/icons/favorite.png"}/>
+    if(isLoading || !dataLoaded){
+        return <div></div>
+    }
+
+    else if(error){
+        return <div>error</div>
+    }
+
+    else {
+        return (
+            data ?
+                <div className={open ? classes.box : classes.unBox}>
+                    <div className={classes.head}>
+                        <img onClick={handlePrevBtn} src={"/images/icons/prevBtn.png"}/>
+                        <span>즐겨찾기한 노트 </span>
+                    </div>
+                    <div className={classes.body}>
+                        <InfiniteScroll
+                            dataLength={data.length}
+                            next={() => handleMoreData()}
+                            hasMore={pageEnd}
+                            style={{
+                                overflow: "scroll",
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "16px"
+                            }}
+                            // loader={<h4>Loading...</h4>}
+                            endMessage={<p>stop</p>}
+                            height={"0"}
+                        >
+                            <div className={classes.scrollDiv}>
+                            {data.map((ele) => {
+                                return (
+                                    <div onClick={() => handlePostDetail(ele)} key={uuidv4()}
+                                         className={classes.content}>
+
+
+                                        <div className={classes.contentBody}>
+                                            <div className={classes.contentTitle}>
+                                                <div className={classes.contentTitleSpan}><span>Q. {ele.title}</span>
+                                                </div>
+                                                <img onClick={(e) => handleIsFavorite(e, ele)}
+                                                     src={"/images/mypage/icons/favorite.png"}/>
+                                            </div>
+                                            <div className={classes.contentMain}><span>{ele.content}</span></div>
+                                            <div className={classes.contentSub}>
+                                                <span>{ele.subCategory.sub_category_name}</span></div>
+                                        </div>
+
+
                                     </div>
-                                    <div className={classes.contentMain}><span>{ele.content}</span></div>
-                                    <div className={classes.contentSub}><span>{ele.subCategory.sub_category_name}</span></div>
-                                </div>
+
+                                )
+                            })}
                             </div>
-                        )
-                    })}
-                </InfiniteScroll>
-            </div>
-        </div>
-        :
-        ""
-    )
+                        </InfiniteScroll>
+
+                    </div>
+                </div>
+                :
+                ""
+        )
+    }
 }
 
 export default FavoriteNote
--- src/component/My/modal/WrittenNote.js
+++ src/component/My/modal/WrittenNote.js
@@ -1,5 +1,5 @@
 import classes from "./WrittenNote.module.css"
-import {useState} from "react";
+import React, {useState} from "react";
 import {useDispatch} from "react-redux";
 import {myPageActions} from "../../../store/myPage-slice";
 import {useEffect} from "react";
@@ -7,12 +7,21 @@ import {v4 as uuidv4} from "uuid";
 import useHttp from "../../../hooks/use-http";
 import {modalActions} from "../../../store/modal-slice";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+
 function WrittenNote(){
     const [open, setOpen] = useState(false)
     const [data, setData] = useState("")
     const dispatch = useDispatch()
 
+    // 무한 스크롤
+    const [pageEnd, setPageEnd] = useState(true)
+    const [pageNum, setPageNum] = useState(0)
+
     const { isLoading, error, sendRequest: getData } = useHttp();
+
+    // 모든 데이터의 로드 확인
+    const [dataLoaded, setDataLoaded] = useState(false);
 
     useEffect(()=>{
         setTimeout(()=> {
@@ -32,51 +41,97 @@ function WrittenNote(){
     }
 
     const handleGetData = () => {
-        getData({url: `http://explorer-cat-api.p-e.kr:8080/api/v1/post/my`}, (taskObj) => {
+        getData({url: `http://explorer-cat-api.p-e.kr:8080/api/v1/post/my?paging_num=${pageNum}&paging_count=20&sortType=desc`}, (taskObj) => {
             setData(taskObj)
+            setDataLoaded(true)
         })
     }
 
     const handlePostDetail = (element) => {
         dispatch(modalActions.changeDetailOpen({open:true, dataId: {id:element.id, mainCategory:element.mainCategory.main_category_id ,subcategory:element.subCategory.sub_category_id}}))
-
     }
 
-    return (
-        data ?
-        <div className={open ? classes.box : classes.unBox}>
-            <div className={classes.head}>
-                <img onClick={handlePrevBtn} src={"/images/icons/prevBtn.png"}/>
-                <span>내가 쓴 노트</span>
-            </div>
-            <div className={classes.body}>
-                {data.map((ele) => {
-                    return (
-                        <div onClick={() => handlePostDetail(ele)} key={uuidv4()} className={classes.content}>
-                            <div className={classes.contentHead}>
-                                <img
-                                     src={ele.approval === 0 ?
-                                    "/images/mypage/icons/approvalReady.png" : ele.approval === 1 ?
-                                    "/images/mypage/icons/approvalCheck.png" :
-                                    "/images/mypage/icons/approvalReject.png"
-                                }/>
-                                <span className={ele.approval === 0 ? classes.readyNote : ele.approval === 1 ? classes.completeNote : classes.rejectNote} >
+    const handleMoreData = () => {
+        getData({url: `http://explorer-cat-api.p-e.kr:8080/api/v1/post/my?paging_num=${pageNum+1}&paging_count=20&sortType=desc`}, (taskObj) => {
+            if(taskObj.length > 0) {
+                const temp = [...data, ...taskObj]
+                setData(temp)
+            }
+            else{
+                setPageEnd(false)
+            }
+        })
+
+        setPageNum((prev) => prev+1)
+    }
+
+    if(isLoading || !dataLoaded){
+        return <div></div>
+    }
+
+    else if(error){
+        return <div>error</div>
+    }
+
+    else {
+        return (
+            data ?
+                <div className={open ? classes.box : classes.unBox}>
+                    <div className={classes.head}>
+                        <img onClick={handlePrevBtn} src={"/images/icons/prevBtn.png"}/>
+                        <span>내가 쓴 노트</span>
+                    </div>
+                    <div className={classes.body}>
+                        <InfiniteScroll
+                            dataLength={data.length}
+                            next={() => handleMoreData()}
+                            hasMore={pageEnd}
+                            style={{
+                                overflow: "scroll",
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "16px"
+                            }}
+                            // loader={<h4>Loading...</h4>}
+                            // endMessage={<p>stop</p>}
+                            height={"0"}
+                        >
+                            <div className={classes.scrollDiv}>
+                            {data.map((ele) => {
+                                return (
+                                    <div onClick={() => handlePostDetail(ele)} key={uuidv4()}
+                                         className={classes.content}>
+                                        <div className={classes.contentHead}>
+                                            <img
+                                                src={ele.approval === 0 ?
+                                                    "/images/mypage/icons/approvalReady.png" : ele.approval === 1 ?
+                                                        "/images/mypage/icons/approvalCheck.png" :
+                                                        "/images/mypage/icons/approvalReject.png"
+                                                }/>
+                                            <span
+                                                className={ele.approval === 0 ? classes.readyNote : ele.approval === 1 ? classes.completeNote : classes.rejectNote}>
                                     {ele.approval === 0 ? "승인대기중" : ele.approval === 1 ? "승인" : "거절"}
                                 </span>
+                                        </div>
+                                        <div className={classes.contentBody}>
+                                            <div className={classes.contentTitle}><span>Q. {ele.title}</span></div>
+                                            <div className={classes.contentMain}><span>{ele.content}</span></div>
+                                            <div className={classes.contentSub}>
+                                                <span>{ele.subCategory.sub_category_name}</span></div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                             </div>
-                            <div className={classes.contentBody}>
-                                <div className={classes.contentTitle}><span>Q. {ele.title}</span></div>
-                                <div className={classes.contentMain}><span>{ele.content}</span></div>
-                                <div className={classes.contentSub}><span>{ele.subCategory.sub_category_name}</span></div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-        :
-        ""
-    )
+                        </InfiniteScroll>
+
+                    </div>
+                </div>
+                :
+                ""
+        )
+    }
 }
 
 export default WrittenNote

193add0ebf4f821b4043d80c88b7f124d337c400
박기찬 <rl0425@naver.com>
2023-04-08 21:53:25 +0900

    마이 페이지 페이지 로딩 예외처리 추가

--- src/component/My/modal/MyInformation.js
+++ src/component/My/modal/MyInformation.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import classes from "./MyInformation.module.css"
 import {myPageActions} from "../../../store/myPage-slice";
 import {useDispatch, useSelector} from "react-redux";
@@ -66,38 +66,50 @@ function MyInformation(){
         }
     }
 
-    return (
-        <div className={open ? classes.box : classes.unBox}>
-            <div className={classes.head}>
-                <img onClick={handlePrevBtn} src={"/images/icons/prevBtn.png"}/>
-                <span>내 정보</span>
+    if(isLoading){
+        return <div></div>
+    }
+
+    else if(error){
+        return <div>error</div>
+    }
+
+    else {
+
+        return (
+            <div className={open ? classes.box : classes.unBox}>
+                <div className={classes.head}>
+                    <img onClick={handlePrevBtn} src={"/images/icons/prevBtn.png"}/>
+                    <span>내 정보</span>
+                </div>
+                <div className={classes.body}>
+                    <div className={classes.profile}>
+                        <img src={profile.image ? profile.image : "/images/icons/noProfile.png"}/>
+                    </div>
+                    <div className={classes.nickname}>
+                        <div className={classes.nicknameHead}>
+                            <span>닉네임</span>
+                        </div>
+                        <div className={classes.nicknameContent}>
+                            <input onChange={(e) => handleContent(e)} onKeyPress={searchEvt}
+                                   placeholder={profile.nickname}/>
+                        </div>
+                    </div>
+                    <div className={classes.email}>
+                        <div className={classes.emailHead}>
+                            <span>이메일</span>
+                        </div>
+                        <div className={classes.emailContent}>
+                            <span>{profile.email}</span>
+                        </div>
+                    </div>
+                    <div onClick={handleSaveNickname} className={change ? classes.submit : classes.unChanged}>
+                        <span>저장하기</span>
+                    </div>
+                </div>
             </div>
-            <div className={classes.body}>
-                <div className={classes.profile}>
-                    <img src={profile.image ? profile.image : "/images/icons/noProfile.png"} />
-                </div>
-                <div className={classes.nickname}>
-                    <div className={classes.nicknameHead}>
-                        <span>닉네임</span>
-                    </div>
-                    <div className={classes.nicknameContent}>
-                        <input onChange={(e) => handleContent(e)} onKeyPress={searchEvt} placeholder={profile.nickname} />
-                    </div>
-                </div>
-                <div className={classes.email}>
-                    <div className={classes.emailHead}>
-                        <span>이메일</span>
-                    </div>
-                    <div className={classes.emailContent}>
-                        <span>{profile.email}</span>
-                    </div>
-                </div>
-                <div onClick={handleSaveNickname} className={change ? classes.submit : classes.unChanged}>
-                    <span>저장하기</span>
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default MyInformation
